23f4903f71cdf1683666d3e23e41a0e65b145234
Evgeniy <n0fr1@mail.ru>
2023-11-12 11:38:34 +0300

    add hook beforeEach

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,4 +1,5 @@
-import { reverseStr, getWithout, getGreater } from '../index.js';
+import _ from 'lodash'
+import { reverseStr, getWithout, getGreater} from '../index.js';
 
 test('reverseStr', () => {
     expect(reverseStr('hello')).toEqual('olleh');
@@ -16,3 +17,32 @@ test('getGreater', () => {
     expect(getGreater(3, 3)).toBe(false);
     expect(getGreater(1, 3)).toBe(false);
 });
+
+let data;
+let dataCopy;
+
+beforeEach(() => {
+  data = {'a': [{'b': {'c': 3}}]};
+  dataCopy = _.cloneDeep(data);
+});
+
+test('plain set', () => {
+  _.set(data, 'a', 'value');
+  dataCopy.a = 'value';
+  expect(data).toEqual(dataCopy);
+});
+
+test('nested set', () => {
+  _.set(data, 'a[0].b.c', true);
+  dataCopy.a[0].b.c = true;
+  expect(data).toEqual(dataCopy);
+});
+
+test('set new property', () => {
+  _.set(data, 'a[0].b.d', false);
+  dataCopy.a[0].b.d = false;
+  expect(data).toEqual(dataCopy);
+});
+
+
+
--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const reverseStr = (str) => str.split('').reverse().join('');
 const getWithout = (arr, ...val) => _.without(arr, ...val);
 const getGreater = (num1, num2) => _.gt(num1, num2);
 
-export { reverseStr, getWithout, getGreater };
+export { reverseStr, getWithout, getGreater};
